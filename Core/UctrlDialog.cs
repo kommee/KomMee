2bd8edd6edabcf0aecc7d2ca26b48829bd4b8794
kommee <kommee@outlook.com>
2012-09-12 12:56:02 +0200

    added raw new usercontrol to Core project

---
+++ Core/UctrlDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KomMee
+{
+    public partial class UctrlDialog : UserControl
+    {
+        public UctrlDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
