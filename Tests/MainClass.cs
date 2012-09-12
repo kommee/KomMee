e1f0ef435fbb9bfa57c424aa9393c3053e473404
kommee <kommee@outlook.com>
2012-09-12 18:17:49 +0200

    New Form for test purposes created and added to Test-Project
    Signed-off-by: kommee <kommee@outlook.com>

--- Tests/MainClass.cs
+++ Tests/MainClass.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+
 
 namespace KomMee_Tests
 {
     public static class MainClass
     {
+        [STAThread]
         public static void Main()
         {
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AdressBookUCtrl());
         }
     }
 }
