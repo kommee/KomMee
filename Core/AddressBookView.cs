d59d163e7da5bbc2cc87cc1fc1d55bc6c008b168
kommee <kommee@outlook.com>
2012-09-12 18:36:17 +0200

    Resource added in test project

---
+++ Core/AddressBookView.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KomMee
+{
+    class AddressBookView : UctrlDialog
+    {
+        public AddressBookView()
+            :base()
+        {
+            // Load addressbook
+            AddressBook adrBook = AddressBook.getInstance();
+
+            // Add Contact to the Listbox
+            foreach (var contact in adrBook.ListOfContacts)
+            {
+                this.LbChoice.Items.Add(contact.Value);
+            }
+            
+        }
+
+        /// <summary>
+        /// Call from Key applay
+        /// </summary>
+        public override void apply()
+        {
+            base.apply();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void cancel()
+        {
+            base.cancel();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void down()
+        {
+            base.down();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void left()
+        {
+            base.left();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void right()
+        {
+            base.right();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void up()
+        {
+            base.up();
+        }
+    }
+}
