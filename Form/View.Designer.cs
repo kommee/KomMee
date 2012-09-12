db26a8e9af39790f73d80f233fd5f3ffab1746d8
kommee <kommee@outlook.com>
2012-09-12 12:37:10 +0200

    Relocated files

--- Form/View.Designer.cs
+++ Form/View.Designer.cs
@@ -28,11 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(View));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.abcdefKeyboardView1 = new KomMee.ABCDEFKeyboardView();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.abcdefKeyboardView1);
+            this.splitContainer1.Size = new System.Drawing.Size(792, 573);
+            this.splitContainer1.SplitterDistance = 305;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // abcdefKeyboardView1
+            // 
+            this.abcdefKeyboardView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.abcdefKeyboardView1.Location = new System.Drawing.Point(3, 3);
+            this.abcdefKeyboardView1.Name = "abcdefKeyboardView1";
+            this.abcdefKeyboardView1.Size = new System.Drawing.Size(760, 265);
+            this.abcdefKeyboardView1.TabIndex = 0;
+            // 
+            // View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "View";
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "View";
+            this.Text = "KomMee";
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private ABCDEFKeyboardView abcdefKeyboardView1;
     }
 }
